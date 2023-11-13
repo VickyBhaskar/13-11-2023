05012092c1786d5627a8c06819393ded0cbe9cdf
VickyBhaskar <vicky2400kumar@gmail.com>
2023-11-13 16:05:00 +0530

    initial Commit

---
+++ Assignment10/JaggedArrayManipulation/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace JaggedArrayManipulation;
+internal class Program
+{
+    static void Main()
+    {
+        // Create a jagged array to store scores of three students
+        int[][] studentScores = new int[][]
+        {
+            new int[] { 85, 92, 78 },
+            new int[] { 90, 87, 93, 89 },
+            new int[] { 76, 88 }
+        };
+
+        // Print scores of each student using nested loops
+        Console.WriteLine("Scores of each student:");
+        for (int i = 0; i < studentScores.Length; i++)
+        {
+            Console.Write($"Student {i + 1}: ");
+            for (int j = 0; j < studentScores[i].Length; j++)
+            {
+                Console.Write($"{studentScores[i][j]} ");
+            }
+            Console.WriteLine();
+        }
+
+        // Calculate and print the average score for each student
+        Console.WriteLine("\nAverage score for each student:");
+        for (int i = 0; i < studentScores.Length; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < studentScores[i].Length; j++)
+            {
+                sum += studentScores[i][j];
+            }
+            double average = sum / studentScores[i].Length;
+            Console.WriteLine($"Student {i + 1}: {average:F2}");
+        }
+
+        // Calculate and print the average score for all students combined
+        Console.WriteLine("\nAverage score for all students combined:");
+        double totalSum = 0;
+        int totalCount = 0;
+        for (int i = 0; i < studentScores.Length; i++)
+        {
+            for (int j = 0; j < studentScores[i].Length; j++)
+            {
+                totalSum += studentScores[i][j];
+                totalCount++;
+            }
+        }
+        double totalAverage = totalSum / totalCount;
+        Console.WriteLine($"Overall Average: {totalAverage:F2}");
+        Console.ReadKey();
+    }
+}
---
+++ Assignment11/Assignment11/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace SalaryCalculationSystem;
+internal class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Employee Salary Calculation System");
+
+        // Choose the type of employee
+        Console.WriteLine("Choose Employee Type:");
+        Console.WriteLine("1. HR");
+        Console.WriteLine("2. Admin");
+        Console.WriteLine("3. Software Developer");
+
+        if (!int.TryParse(Console.ReadLine(), out int employeeType))
+        {
+            Console.WriteLine("Invalid input for employee type. Please enter a valid number.");
+            return;
+        }
+
+        // Call the CalculateSalary method based on the chosen employee type
+        switch (employeeType)
+        {
+            case 1:
+                if (GetInput(out int hrWorkingHours, "Enter working hours") &&
+                    GetInput(out int hrWorkingDays, "Enter number of working days"))
+                {
+                    double hrSalary = CalculateSalary(hrWorkingHours, hrWorkingDays);
+                    Console.WriteLine($"Monthly Salary for HR: ${hrSalary}");
+                }
+                break;
+
+            case 2:
+                if (GetInput(out int adminWorkingHours, "Enter working hours") &&
+                    GetInput(out int adminWorkingDays, "Enter number of working days") &&
+                    GetInput(out int projectHandles, "Enter number of project handles"))
+                {
+                    double adminSalary = CalculateSalary(adminWorkingHours, adminWorkingDays, projectHandles);
+                    Console.WriteLine($"Monthly Salary for Admin: ${adminSalary}");
+                }
+                break;
+
+            case 3:
+                if (GetInput(out int devWorkingHours, "Enter working hours") &&
+                    GetInput(out int devWorkingDays, "Enter number of working days") &&
+                    GetInput(out int devProjectHandles, "Enter number of project handles") &&
+                    GetInput(out int devExtras, "Enter extra tasks"))
+                {
+                    double devSalary = CalculateSalary(devWorkingHours, devWorkingDays, devProjectHandles, devExtras);
+                    Console.WriteLine($"Monthly Salary for Software Developer: ${devSalary}");
+                }
+                break;
+
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+        Console.ReadKey();
+    }
+
+    // Method to calculate monthly salary with optional parameters
+    static double CalculateSalary(int workingHours, int workingDays, int projectHandles = 0, int extras = 0)
+    {
+        return workingHours * workingDays * 100 + projectHandles * 3000 + extras * 2000;
+    }
+
+    // Method to get input from the user 
+    static bool GetInput(out int result, string prompt)
+    {
+        Console.Write($"{prompt}: ");
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            return false;
+        }
+    }
+}
